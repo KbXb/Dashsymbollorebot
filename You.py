397d501db9889e87d6a31cec618110059a2e6875
KbXb <143540231+KbXb@users.noreply.github.com>
2023-08-29 19:48:49 -0300

    Create You.py

---
+++ You.py
@@ -0,0 +1,9 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="oEBJxmShVGOoJJiUVB24zQ",
+    client_secret="CLIENT",
+    password="PASSWORD",
+    user_agent="LMGTFY (by u/USERNAME)",
+    username="USERNAME",
+)
